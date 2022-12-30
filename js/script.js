9338bcf5145d7fb3bb9ea49e9ac6ba27a54eda4b
Tetiana Breslavska <tvk.koba@gmail.com>
2022-12-30 23:00:18 +0100

    Add change value

---
+++ js/script.js
@@ -0,0 +1,6 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `kamień`;
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + computerMove + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
