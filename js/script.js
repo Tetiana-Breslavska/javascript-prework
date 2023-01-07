3c1754e4284d366c4c5921cb04e27173173a6d6e
Tetiana Breslavska <tvk.koba@gmail.com>
2023-01-07 14:38:50 +0100

    Improved the code

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 function playGame(playerInput) {
   function getMoveName(argMoveId) {
-    if (argMoveId == 1) {
+    if (argMoveId === 1) {
       return "kamień";
-    } else if (argMoveId == 2) {
+    } else if (argMoveId === 2) {
       return "papier";
-    } else if (argMoveId == 3) {
+    } else if (argMoveId === 3) {
       return "nożyce";
     }
 
-    printMessage("Nie znam ruchu o id " + argMoveId + ".");
-    return "nieznany ruch";
+    // printMessage("Nie znam ruchu o id " + argMoveId + ".");
+    // return "nieznany ruch";
   }
 
   function displayResult(argComputerMove, argPlayerMove) {
     console.log("moves:", argComputerMove, argPlayerMove);
 
     if (
-      (argComputerMove == "kamień" && argPlayerMove == "papier") ||
-      (argComputerMove == "papier" && argPlayerMove == "nożyce") ||
-      (argComputerMove == "nożyce" && argPlayerMove == "kamień")
+      (argComputerMove === "kamień" && argPlayerMove === "papier") ||
+      (argComputerMove === "papier" && argPlayerMove === "nożyce") ||
+      (argComputerMove === "nożyce" && argPlayerMove === "kamień")
     ) {
       printMessage("You win!");
       counterPlayer += 1;
     } else if (
-      (argComputerMove == "papier" && argPlayerMove == "kamień") ||
-      (argComputerMove == "kamień" && argPlayerMove == "nożyce") ||
-      (argComputerMove == "nożyce" && argPlayerMove == "papier")
+      (argComputerMove === "papier" && argPlayerMove === "kamień") ||
+      (argComputerMove === "kamień" && argPlayerMove === "nożyce") ||
+      (argComputerMove === "nożyce" && argPlayerMove === "papier")
     ) {
       printMessage("You lost!");
       counterComp += 1;
     } else if (
-      (argComputerMove == "kamień" && argPlayerMove == "kamień") ||
-      (argComputerMove == "papier" && argPlayerMove == "papier") ||
-      (argComputerMove == "nożyce" && argPlayerMove == "nożyce")
+      (argComputerMove === "kamień" && argPlayerMove === "kamień") ||
+      (argComputerMove === "papier" && argPlayerMove === "papier") ||
+      (argComputerMove === "nożyce" && argPlayerMove === "nożyce")
     ) {
       printMessage("Played a draw!");
     } else {
@@ -45,23 +45,25 @@ function playGame(playerInput) {
 
   let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-  console.log("Wylosowana liczba to: " + randomNumber);
-
   let computerMove = getMoveName(randomNumber);
 
   printMessage("Snowman:" + computerMove);
-
-  console.log("Gracz wpisał: " + playerInput);
 
   let playerMove = getMoveName(playerInput);
 
   printMessage("You:" + playerMove);
 
   displayResult(computerMove, playerMove);
+
   console.log(counterComp + ":" + counterPlayer);
+
   printResult(counterComp + ":" + counterPlayer);
 }
 console.log("hello");
+
+let counterComp = 0;
+
+let counterPlayer = 0;
 
 document.getElementById("play-rock").addEventListener("click", function () {
   playGame(1);
@@ -73,5 +75,4 @@ document.getElementById("play-scissors").addEventListener("click", function () {
   playGame(3);
 });
 
-let counterComp = 0;
-let counterPlayer = 0;
+
